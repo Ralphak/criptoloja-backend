c6209570875f672ec112ae4102bdd6bacaaf38ff
Rafael Alves <ralphawk@gmail.com>
2021-12-09 10:24:58 -0300

    hotfix: conexão ssl com o banco de produção

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const connection = new Sequelize(process.env.DATABASE_URL);
+
+const ssl = process.env.DATABASE_URL.includes("localhost") ? undefined : { require: true, rejectUnauthorized: false };
+const connection = new Sequelize(process.env.DATABASE_URL, { dialectOptions: { ssl } });
 
 module.exports = {
     connection,
     DataTypes,
-    testConnection: () => connection.authenticate().then(()=>"OK").catch(error => {
+    testConnection: () => connection.authenticate().then(() => "OK").catch(error => {
         console.error(error);
         return error.toString();
     })
