29762ae46182b26f6e2e20fe8aa44e33a9c191ff
Rafael Alves <ralphawk@gmail.com>
2022-04-03 16:39:04 -0300

    Habilitar CORS

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const fs = require("fs");
 if (fs.existsSync(".env")) require("dotenv").config();
 
 const app = require("express")();
+const cors = require("cors");
+app.use(cors({
+    origin: ["https://criptoloja.herokuapp.com", "http://localhost:3000", "http://192.168.0.3:3000"]
+}));
 app.use(require("body-parser").json());
 app.use(require("./src/routes"));
+
 app.listen(process.env.PORT || 5000, async() => {
     if (process.argv.includes("run-tests")){
         await require("jest").run([
